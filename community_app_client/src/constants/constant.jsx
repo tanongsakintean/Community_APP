c7aec37ff784750f6c3945625c4d7cd6dca0b135
tanongsak intean <tanongsakintean.5333@gmail.com>
2023-01-04 15:28:25 +0700

    chenge baseUrl and baseImg

--- community_app_client/src/constants/constant.jsx
+++ community_app_client/src/constants/constant.jsx
@@ -1,5 +1,5 @@
 export const baseUrl =
-  "http://localhost:8888/Community_project/Community_app_server/api";
+  "http://localhost:8888/COMMUNITY_APP/Community_app_server/api";
 
 export const baseImg =
-  "http://localhost:8888/Community_project/Community_app_server/images/";
+  "http://localhost:8888/COMMUNITY_APP/Community_app_server/images/";
